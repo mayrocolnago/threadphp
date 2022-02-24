2ab5c62ce0f035e609fdb8b682d538a9beea4063
Mayro Colnago <mayrocolnago@gmail.com>
2022-02-24 20:36:56 -0300

    Update thread.php

--- thread.php
+++ thread.php
@@ -1,18 +1,18 @@
 <?php
-function thread($func) {
+function thread($func,$variables=[]) {
     if(!function_exists('shell_exec')) return null;
+    if(!is_array($variables)) $variables = [];
     $refl = new \ReflectionFunction($func); $rp = [];
     $code = explode("\n", file_get_contents($refl->getFileName()));
     $code = array_slice($code, ($begin = $refl->getStartLine()-1), (($refl->getEndLine())-($begin)));
     $code = str_replace((explode('{',implode("\n",$code).'{')[0] ?? '').'{','',$code);
     $code = str_replace('}'.(($lc = explode('}','}'.implode("\n",$code)))[count($lc)-1] ?? ''),'',$code);
-    $trgg = "nohup setsid php ";
-    if(!empty($params = $refl->getParameters()) && is_array($vars = get_defined_vars()))
+    $trgg = "nohup setsid ".PHP_BINDIR."/php ".(($inipath = php_ini_loaded_file()) ? "-c $inipath " : "");
+    if(!empty($params = $refl->getParameters()) && is_array($dfvars = get_defined_vars()))
       foreach($params as $k => $v)
-        if(!empty($k = ($v->name ?? null)))
-          if(!empty($hq = http_build_query([$k => ($vars[$k] ?? ($_REQUEST[$k] ?? ($_SERVER[$k] ?? null)))])))
-            $trgg .= "-d $hq ";
-    shell_exec($bash = ($trgg."-r \"".str_replace('"',"\\\"",implode(" \\\n",$code))."\" > /dev/null 2>&1 &"));
+        if(!empty($k = ($v->name ?? null)) && !isset($variables[$k])) 
+          $variables[$k] = ($dfvars[$k] ?? ($_REQUEST[$k] ?? ($_SERVER[$k] ?? null)));
+    shell_exec($bash = ($trgg."-r \"@parse_str(urldecode('".urlencode(http_build_query($variables))."')); ".str_replace('"',"\\\"",implode(" \\\n",$code))."\" > /dev/null 2>&1 &"));
     return $bash;
 }
 ?>
